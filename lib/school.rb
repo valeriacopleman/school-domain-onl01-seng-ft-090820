59363db4dbe17e43f7e7e006ca0650915f53d6ed
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-18 04:19:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+    
+  def roster
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+  
+   @roster[grade]= []
+   @roster[grade] << name 
+  end
+  
+  def grade=(grade)
+    @grade = grade
+  end
+  
+  def grade
+    @grade
+  end
+  
+end
