6eb227fb3f66555c3e4628862c49f554626911b2
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-18 05:32:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+  
+  attr_accessor :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    if @roster[level] 
+      @roster[level] << student
+    else
+      @roster[level] = []
+      @roster[level] << student 
+  end
+  end
+  
+  def grade(level)
+    @roster.collect do |keys, values|
+      if keys==level 
+         return values
+      end
+    end
+  end
+  
+  def sort 
+    sort_hash = {}
+    @roster.each do |keys, values|
+      sort_hash[keys] = values.sort 
+    end
+    sort_hash
+  end
+end
