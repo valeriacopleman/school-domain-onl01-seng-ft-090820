4e4cfb246d5708c69141cb898beb58d43f35055e
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-18 04:54:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,7 @@ class School
     if @roster[grade]
       @roster[grade] << student
     else
-      @roster[grade] || = []
+      @roster[grade] ||= []
       @roster[grade] << student 
   end
   end
